903c1b14a855c721d983a1ce5731360397bef2a5
Miska Savenius <miska.savenius@gmail.com>
2025-01-16 15:45:44 +0200

    Nimen muutos funktioon

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     convert_ids_to_int(data, "customer_id")
     convert_ids_to_int(data, "order_id")
     fill_float_if_empty(data, "quantity")
-    remove_if_empty(data, "quantity")
+    remove_if_zero(data, "quantity")
     fill_float_if_empty(data, "unit_price")
     fill_unknow_on_empty(data, "product_category")
     fill_unknow_on_empty(data, "country")
@@ -29,7 +29,7 @@ def convert_ids_to_int(df: pd.DataFrame, key: str):
 def fill_float_if_empty(df: pd.DataFrame, key: str):
     df[key] = df[key].fillna(0.00)
 
-def remove_if_empty(df: pd.DataFrame, key: str):
+def remove_if_zero(df: pd.DataFrame, key: str):
     df.drop(index=df[df[key] == 0].index, inplace=True)
 
 def fill_unknow_on_empty(df: pd.DataFrame, key: str):
