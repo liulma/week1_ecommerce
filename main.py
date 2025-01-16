205f98fd4c3a7eb13d998870ea0a6edd392c4964
liulma <linda.ulma@skillio.ai>
2025-01-16 15:19:21 +0200

    Added function data_manipulation

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ def main():
     data = read_file("ecommerce_sales (1).csv")
     print(data.head(10))
 
+    manipulated_data = data_manipulation(data)
+    print(manipulated_data)
 
 def read_file(file_name: str):
     return pd.read_csv(file_name)
+
+def data_manipulation(data):
+    data['total_price'] = data['quantity'] * data['unit_price']
+    data['order_date'] = pd.to_datetime(data['order_date']) 
+    data['order_weekday'] = data['order_date'].dt.day_name()
+    data['order_month'] = data['order_date'].dt.month
+
+    return data
+
 
 main()
